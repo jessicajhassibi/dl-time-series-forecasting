d1846de595f72a728c17001d19001ed9b4fc1818
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2026-06-05 15:34:32 +0200

    Add cli arguments for the training script

--- submission/run_training.py
+++ submission/run_training.py
@@ -1,26 +1,42 @@
 import os.path
 from datetime import datetime
 
+import tyro
+
 from src.datasets import load_dataset, ForecastDataset
 from src.linear import LinearModel
 from src.train import train_model
 
-if __name__ == "__main__":
-    prediction_horizon = 336
-    context_size = 3 * prediction_horizon
 
-    model_name = "linear"
-    model = LinearModel(context_size, prediction_horizon)
-    is_shifted_output = False
+def run_training(model_name: str = "linear", context_size: int = 336 * 3, prediction_horizon: int = 336,
+                 num_epochs: int = 1):
+    """Run training for the given model
+
+    Args:
+        model_name: name of the model to train
+        context_size: size of the context for the model to use
+        prediction_horizon: size of the model prediction
+        num_epochs: number of epochs to train the model
+    """
+    if model_name == "linear":
+        model = LinearModel(context_size, prediction_horizon)
+        is_shifted_output = False
+    else:
+        print(f"Unknown model name {model_name}")
+        return
 
     train_df, metadata = load_dataset("train")
     train_dataset = ForecastDataset(train_df, metadata, context_size=context_size,
                                     prediction_horizon=prediction_horizon,
                                     is_shifted_output=is_shifted_output)
-    print(f"Length of the dataset is {len(train_dataset)}")
+    print(f"Loaded training dataset of length {len(train_dataset)}")
 
     log_dir = os.path.join("logs", model_name, datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
     os.makedirs(log_dir, exist_ok=True)
     print(f"Writing experiment data to {log_dir}")
 
-    train_model(model, train_dataset, log_dir, num_epochs=3)
+    train_model(model, train_dataset, log_dir, num_epochs=num_epochs)
+
+
+if __name__ == "__main__":
+    tyro.cli(run_training)
