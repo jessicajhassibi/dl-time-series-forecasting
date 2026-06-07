cd37149df3aefc17f50f3b7b41fda8b56c4c68eb
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2026-06-07 22:19:00 +0200

    Include predictions from existing output files in the plot

--- submission/plot_predictions.py
+++ submission/plot_predictions.py
@@ -1,11 +1,22 @@
 import sys
+from pathlib import Path
 
-from src.config import get_configuration_id
-from src.plot import plot_prediction_comparison
+import pandas as pd
+
 from src.baselines import make_all_baselines
+from src.config import get_configuration_id
 from src.datasets import load_dataset, load_metadata, Schema
+from src.plot import plot_prediction_comparison
 from src.plot import plot_series
 from src.predict import find_last_checkpoint, predict_for_checkpoint
+
+
+def load_previous_predictions(parent_dir: Path = Path("predictions")) -> dict[str, pd.DataFrame]:
+    result = {}
+    for prediction_file in list(parent_dir.glob("**/*.csv")):
+        result["file:" + prediction_file.with_suffix("").name] = pd.read_csv(prediction_file)
+    return result
+
 
 if __name__ == "__main__":
     log_dir = "logs"
@@ -21,14 +32,14 @@ if __name__ == "__main__":
     metadata = load_metadata()
     schema = Schema.from_metadata(metadata)
 
-    # TODO maybe read predictions from predictions directory instead of running a model
     model_result_dict = predict_for_checkpoint(checkpoint_path, train_df, val_df, schema)
     model_id = get_configuration_id(model_result_dict["config"])
     model_result = model_result_dict["result"]
 
     baselines_results = make_all_baselines(train_df, val_df)
     all_results = dict(baselines_results,
-                       **{model_id: model_result})
+                       **load_previous_predictions(),
+                       **{"last:" + model_id: model_result})
 
     plot_series(model_result, title=f"Predictions for {model_id} model",
                 x_key="timestamp", y_key="prediction", num_series=-1)
