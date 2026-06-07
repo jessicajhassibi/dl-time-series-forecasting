671533c1788c0813f574c805e4c2447421c53734
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2026-06-07 22:33:22 +0200

    Do not fail if no checkpoints found

--- submission/plot_predictions.py
+++ submission/plot_predictions.py
@@ -1,4 +1,3 @@
-import sys
 from pathlib import Path
 
 import pandas as pd
@@ -18,29 +17,33 @@ def load_previous_predictions(parent_dir: Path = Path("predictions")) -> dict[st
     return result
 
 
-if __name__ == "__main__":
-    log_dir = "logs"
+def predict_with_last_checkpoint(log_dir: str = "logs") -> dict[str, pd.DataFrame]:
     checkpoint_path = find_last_checkpoint(log_dir)
     if not checkpoint_path:
         print(f"Could not find checkpoint to use in {log_dir}")
-        sys.exit(1)
+        return {}
 
-    print(f"Using checkpoint {checkpoint_path}")
+    print(f"Using last checkpoint: {checkpoint_path}")
+    model_result_dict = predict_for_checkpoint(checkpoint_path, train_df, val_df, schema)
+    model_id = get_configuration_id(model_result_dict["config"])
+    model_result = model_result_dict["result"]
+    return {model_id: model_result}
 
+
+if __name__ == "__main__":
     train_df = load_dataset("train")
     val_df = load_dataset("validation")
     metadata = load_metadata()
     schema = Schema.from_metadata(metadata)
 
-    model_result_dict = predict_for_checkpoint(checkpoint_path, train_df, val_df, schema)
-    model_id = get_configuration_id(model_result_dict["config"])
-    model_result = model_result_dict["result"]
-
     baselines_results = make_all_baselines(train_df, val_df)
+    previous_results = load_previous_predictions()
+    model_results = predict_with_last_checkpoint()
     all_results = dict(baselines_results,
-                       **load_previous_predictions(),
-                       **{"last:" + model_id: model_result})
+                       **previous_results,
+                       **model_results)
 
-    plot_series(model_result, title=f"Predictions for {model_id} model",
-                x_key="timestamp", y_key="prediction", num_series=-1)
+    for model_name, model_prediction in model_results.items():
+        plot_series(model_prediction, title=f"Predictions for {model_name}",
+                    x_key="timestamp", y_key="prediction", num_series=-1)
     plot_prediction_comparison(all_results, schema, "unit_000")
