0f5b87e78e5232c778990ff6f703bd32b006dfb1
Julia Beliaeva <juliabeliaeva@users.noreply.github.com>
2026-07-20 15:19:33 +0200

    Use context manager for evaluation

--- submission/src/train.py
+++ submission/src/train.py
@@ -1,5 +1,6 @@
 """Training functions."""
 import os.path
+from contextlib import contextmanager
 from pathlib import Path
 
 import pandas as pd
@@ -108,6 +109,26 @@ def save_model(model: Module, optimizer: Optimizer, epoch: int, global_step: int
                os.path.join(log_dir, f"checkpoint-{global_step}.pt"))
 
 
+@contextmanager
+def evaluate(model: Module):
+    """Switch the model to evaluation mode and disable gradients.
+
+    Usage:
+
+    .. code-block:: python
+
+        with evaluate(model):
+            # compute metrics
+    """
+    training_mode = model.training
+    model.eval()
+    try:
+        with torch.no_grad():
+            yield
+    finally:
+        model.train(mode=training_mode)
+
+
 def get_validation_metrics(model: Module, dataset: Dataset[ForecastSample],
                            batch_size: int = 500) -> dict[str, float | int]:
     """
@@ -120,10 +141,7 @@ def get_validation_metrics(model: Module, dataset: Dataset[ForecastSample],
     Returns:
         dictionary containing metrics names and metric values
     """
-    training_mode = model.training
-    model.eval()
-
-    with torch.no_grad():
+    with evaluate(model):
         val_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
         sum_error = 0
@@ -138,8 +156,6 @@ def get_validation_metrics(model: Module, dataset: Dataset[ForecastSample],
             sum_values = sum_values + torch.sum(torch.abs(y)).item()
         # TODO add additional metrics
         wape = sum_error / sum_values
-
-    model.train(mode=training_mode)
 
     return dict(wape=wape)
 
@@ -162,10 +178,7 @@ def get_long_horizon_validation_metrics(model: Module, context_size: int, predic
                        Can be greater than the model prediction horizon to test if the model can make long-term predictions.
     Returns:
         Dictionary containing metrics names and metric values."""
-    training_mode = model.training
-    model.eval()
-
-    with torch.no_grad():
+    with evaluate(model):
         series_ids = schema.get_series_ids(dataframe)
         series_groups = schema.get_series_groups(dataframe)
         context_df = series_groups.head(prediction_start)
@@ -178,7 +191,5 @@ def get_long_horizon_validation_metrics(model: Module, context_size: int, predic
         assert predicted_values.shape == actual_values.shape
 
         wape = torch.sum(torch.abs(predicted_values - actual_values)) / torch.sum(torch.abs(actual_values)).item()
-
-    model.train(mode=training_mode)
 
     return dict(wape=wape)
